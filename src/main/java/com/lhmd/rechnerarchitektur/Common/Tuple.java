f29312e85c2edea41ea98ac78feafa1ba93ac9fd
midare160 <43279447+midare160@users.noreply.github.com>
2025-03-27 11:02:27 +0100

    Create Tuple.java

---
+++ src/main/java/com/lhmd/rechnerarchitektur/Common/Tuple.java
@@ -0,0 +1,7 @@
+package com.lhmd.rechnerarchitektur.Common;
+
+public record Tuple<T1, T2>(T1 item1, T2 item2) {
+    public static <T1, T2> Tuple<T1, T2> of(T1 t1, T2 t2) {
+        return new Tuple<>(t1, t2);
+    }
+}
