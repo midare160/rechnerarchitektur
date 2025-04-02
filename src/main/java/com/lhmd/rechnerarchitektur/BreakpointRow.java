519fd23b9c111ba1a8290666ab62eea343adad9a
midare160 <43279447+midare160@users.noreply.github.com>
2025-04-03 01:44:48 +0200

    Fix CSS

--- src/main/java/com/lhmd/rechnerarchitektur/BreakpointRow.java
+++ src/main/java/com/lhmd/rechnerarchitektur/BreakpointRow.java
@@ -1,19 +1,28 @@
 package com.lhmd.rechnerarchitektur;
 
 import com.lhmd.rechnerarchitektur.instructions.Instruction;
+import javafx.css.PseudoClass;
 import javafx.scene.control.TableRow;
 
 public class BreakpointRow extends TableRow<Instruction> {
+
+    private static final PseudoClass BREAKPOINT_ACTIVE = PseudoClass.getPseudoClass("breakpoint-active");
+
+    @Override
+    protected void updateItem(Instruction instruction, boolean empty) {
+        super.updateItem(instruction, empty);
+
+        var isActive = !empty && instruction != null && instruction.isBreakpointActive();
+
+        pseudoClassStateChanged(BREAKPOINT_ACTIVE, isActive);
+    }
 
     @Override
     public void updateIndex(int i) {
         super.updateIndex(i);
 
-        if (i < 0 || getItem() == null || !getItem().isBreakpointActive()) {
-            setStyle("");
-            return;
-        }
+        var isActive = i >= 0 && getItem() != null && getItem().isBreakpointActive();
 
-        setStyle("-fx-background-color: #40252b;");
+        pseudoClassStateChanged(BREAKPOINT_ACTIVE, isActive);
     }
 }
